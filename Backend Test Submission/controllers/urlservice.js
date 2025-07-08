881c52eeccd378e560ed586022f408e8fd3716d8
Prabhjeet47 <prabhsmanku@gmail.com>
2025-07-08 12:47:24 +0530

    working create short url function

--- Backend Test Submission/controllers/urlservice.js
+++ Backend Test Submission/controllers/urlservice.js
@@ -1,11 +1,11 @@
 import logmaker from "../../Logging Middleware/logMaker.js";
 import urlModel from "../models/url.js";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 export const createUrlShortner = async (req, res) => {
   try {
-    //create url shortner service
-    console.log(req.body);
-
     const {url, validity = 30, shortcode} = req.body;
 
     //if no url provided by the user
@@ -19,33 +19,78 @@ export const createUrlShortner = async (req, res) => {
       return res.status(400).json({msg: "please provide URL"});
     }
 
-    //if the user has provided its own secret code and expiry time
-    if (url && shortcode && validity) {
+    //if the user has provided its own short code
+    if (shortcode) {
+      const exists = await urlModel.findOne({shortcode});
+      if (exists) {
+        await logmaker(
+          "backend",
+          "error",
+          "handler",
+          "Shortcode already exists"
+        );
+        return res.status(409).json({msg: "Shortcode already in use"});
+      }
+      await urlModel.create({
+        shortcode: shortcode,
+        defaulturl: url,
+        validity: validity,
+      });
+
       await logmaker(
         "backend",
         "info",
         "handler",
-        "short url created with an expiry time"
+        `Custom shortcode '${shortcode}' created`
       );
 
-      return res
-        .status(201)
-        .json({shortlink: `http://localhost`, expiry: `something`});
+      const date = new Date(validity);
+      const properDate = date.toISOString().split(".")[0] + "Z";
+
+      return res.status(201).json({
+        shortlink: `http://localhost:${process.env.PORT}/${shortcode}`,
+        expiry: properDate,
+      });
     }
 
-    //if the user has provided its own secret code and not provided expiry time
-    if (url && shortcode) {
-      await logmaker("backend", "info", "handler", "short url created");
-      return res
-        .status(201)
-        .json({shortlink: `/${shortcode}`, expiry: `something`});
+    //if the user has not given short code
+    if (!shortcode) {
+      const chars =
+        "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+      let generated;
+
+      //to create random short code of length 7
+      while (true) {
+        generated = "";
+        for (let i = 0; i < 7; i++) {
+          generated += chars.charAt(Math.floor(Math.random() * chars.length));
+        }
+
+        const existing = await urlModel.findOne({shortcode: generated});
+        if (!existing) break;
+      }
+
+      await urlModel.create({
+        shortcode: generated,
+        defaulturl: url,
+        validity: validity,
+      });
+
+      await logmaker(
+        "backend",
+        "info",
+        "handler",
+        `Custom shortcode '${shortcode}' created`
+      );
+
+      const date = new Date(validity);
+      const properDate = date.toISOString().split(".")[0] + "Z";
+
+      return res.status(201).json({
+        shortlink: `http://localhost:${process.env.PORT}/${generated}`,
+        expiry: properDate,
+      });
     }
-
-    //if the user has not provided its own secret code
-    const chars =
-      "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-
-    for (const i = 0; i < 7; i++) {}
   } catch (err) {
     console.log(err);
     logmaker("backend", "fatal", "handler", "internal error");
--- Backend Test Submission/index.js
+++ Backend Test Submission/index.js
@@ -12,7 +12,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
-connectDb;
+connectDb();
 
 //routes
 app.get("/", (req, res) => {
