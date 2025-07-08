03af60af8541d84b91812ed8fc2cb04cb7e42762
Prabhjeet47 <prabhsmanku@gmail.com>
2025-07-08 11:03:50 +0530

    created log maker function

---
+++ Backend Test Submission/index.js
@@ -0,0 +1,25 @@
+import express from "express";
+import dotenv from "dotenv";
+import logmaker from "../Logging Middleware/logMaker.js";
+
+dotenv.config();
+
+const app = express();
+
+//middlewares
+app.use(express.json());
+
+//routes
+app.get("/", (req, res) => {
+  logmaker(
+    "backend",
+    "info",
+    "route",
+    "Hello response from server on home page route- /"
+  );
+  return res.send("hello from server");
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`server running on http://localhost:${process.env.PORT}`);
+});
--- Logging Middleware/logMaker.js
+++ Logging Middleware/logMaker.js
@@ -1,5 +1,15 @@
 import axios from "axios";
 
-export default function LogMaker() {
-  //make logs here
+export default async function LogMaker(stack, level, p, message) {
+  //make logs here by sending post request to the below URL
+  try {
+    await axios.post("http://20.244.56.144/evaluation-service/logs", {
+      stack: stack,
+      level: level,
+      package: p,
+      message: message,
+    });
+  } catch (err) {
+    console.log(err);
+  }
 }
